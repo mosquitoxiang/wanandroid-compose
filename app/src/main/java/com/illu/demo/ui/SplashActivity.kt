3ef5dc05624055b356e40a9a6cb7d00a80587de9
mos <337280502@qq.com>
2020-09-10 23:09:14 +0800

    xczxas

--- app/src/main/java/com/illu/demo/ui/SplashActivity.kt
+++ app/src/main/java/com/illu/demo/ui/SplashActivity.kt
@@ -1,33 +1,20 @@
 package com.illu.demo.ui
 
-import android.os.Bundle
 import com.illu.demo.base.BaseActivity
 import com.illu.baselibrary.core.ActivityHelper
 import com.illu.demo.R
-import com.illu.demo.test.Name
-import com.illu.demo.test.PieView
-import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : BaseActivity() {
-
-    private var list = mutableListOf(
-        Name("qq", 20),
-        Name("ww", 30),
-        Name("ee", 40),
-        Name("rr", 50)
-    )
 
     override fun getLayoutId(): Int {
         return R.layout.activity_splash
     }
 
     override fun initView() {
-//        val pv = PieView(this)
-        pv.setData(list)
-//        postDelayed(Runnable {
-//            ActivityHelper.start(MainActivity::class.java)
-//            ActivityHelper.finish(SplashActivity::class.java)
-//        }, 1000)
+        postDelayed(Runnable {
+            ActivityHelper.start(MainActivity::class.java)
+            ActivityHelper.finish(SplashActivity::class.java)
+        }, 1000)
     }
 
     override fun initData() {
